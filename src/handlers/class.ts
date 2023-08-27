511c0f0ce661008a0d31f4d8f7411cf011286c1f
Brian Adams <bpeteradams@gmail.com>
2023-08-27 00:13:18 -0400

    Feature(classes): added some logging to the class feature

--- src/handlers/class.ts
+++ src/handlers/class.ts
@@ -1,15 +1,17 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { findAllClasses } from "../services/class.js";
 
-export async function handleAllClasses(_request: FastifyRequest, reply: FastifyReply) {
+export async function handleAllClasses(request: FastifyRequest, reply: FastifyReply) {
 	const classes = await findAllClasses()
 	if(classes.data === null) {
+		request.log.error(classes.error)
 		reply.code(404)
 		return {
 			data: "There Was An Error",
 			code: 404
 		}
 	}
+request.log.info(classes.data)
 reply.code(200)
 	return {
 		data: classes.data,
