c408ca20a47157b1a315096f7e81cb39b2120d44
thebadams <bpeteradams@gmail.com>
2023-08-29 22:59:40 -0400

    Feature(classes): Added a basic ranged attack row and seeded; added a schema for validation of source data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,31 @@
-import {readFile} from 'fs/promises'
-import path from 'path'
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 async function getClasses() {
-	const file = path.join(process.cwd(), 'data/classes.json')
+	const file = path.join(process.cwd(), 'data/classes.json');
 	const res = await readFile(file, 'utf8');
 	const data = JSON.parse(res);
-	console.log(data);
-	return data;
+	console.log(data.classes);
+	return data.classes;
 }
 
 async function seedClasses() {
-	const classData = await getClasses()
+	const classData = await getClasses();
 	try {
-		void await prisma.class.deleteMany({})
-		const classes = await prisma.class.createMany({data: classData})
-		console.log(classes)
+		void (await prisma.class.deleteMany({}));
+		const classes = await prisma.class.createMany({ data: classData });
+		console.log(classes);
 	} catch (error) {
-		console.error(error)
+		console.error(error);
 	} finally {
-		await prisma.$disconnect()
+		await prisma.$disconnect();
 	}
 }
 
 async function seed() {
-	void await seedClasses()
+	void (await seedClasses());
 }
 
-seed()
+seed();
