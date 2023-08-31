295dc5ec47e42d23e40ff4e4ad360bbd671c4c68
Brian Adams <bpeteradams@gmail.com>
2023-08-30 21:30:17 -0400

    Feature(drizzle): Cleanup: uninstalled prisma and ts-node

--- src/services/class.ts
+++ src/services/class.ts
@@ -1,35 +1,5 @@
-import { prisma } from "../config/prisma.js";
 import { drizzleDB } from "../config/drizzle.js";
 import { classes } from "../schema.js";
-
-export type classFieldFilter = {
-  name?: boolean;
-  abilBonus?: boolean;
-  baseHP?: boolean;
-  recoveryDice?: boolean;
-  startingGold?: boolean;
-  noArmor?: boolean;
-  lightArmor?: boolean;
-  heavyArmor?: boolean;
-  shield?: boolean;
-  basicMeleeAttack?: boolean;
-  basicRangedAttack?: boolean;
-};
-
-export async function findAllClasses(filter: classFieldFilter) {
-  try {
-    const classes = await prisma.class.findMany({ select: filter });
-    return {
-      data: classes,
-      error: null,
-    };
-  } catch (error) {
-    return {
-      data: null,
-      error: error,
-    };
-  }
-}
 
 export async function drizzleAllClasses() {
   try {
