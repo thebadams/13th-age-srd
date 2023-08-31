aba5d12c8c71f95f66c31250ca9136b9eb684229
Brian Adams <bpeteradams@gmail.com>
2023-08-30 21:25:13 -0400

    Feature(scripts): Added tsx as a way to handle a dev server and scripts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ServerConfig } from './config/server.js';
-import { createServer } from './server.js';
+import { ServerConfig } from "./config/server.js";
+import { createServer } from "./server.js";
 
-const {server, start, port} = await createServer(ServerConfig)
+const { server, start, port } = await createServer(ServerConfig);
 
-void await start(server, port)
+void (await start(server, port));
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,24 @@
-import Fastify, { FastifyInstance } from 'fastify';
-import type { ServerConfig } from './config/server.js';
-import { registerClassRoutes } from './routes/class.js';
-
+import Fastify, { FastifyInstance } from "fastify";
+import type { ServerConfig } from "./config/server.js";
+import { registerClassRoutes } from "./routes/class.js";
 
 export async function createServer(config: ServerConfig) {
-	const PORT = config.port;
-	const server = Fastify({ logger: true });
-	server.get('/', (_req, _reply) => {
-		return {
-			message: "Hello World"
-		}
-	})
-	
-	void await registerClassRoutes(server)
-	const start = async (server: FastifyInstance, port: number) => {
-		try {
-			void await server.listen({port});
-		} catch (error) {
-			console.error(error)
-			process.exit(2)
-		}
-	};
-	return {server, start, port: PORT}
+  const PORT = config.port;
+  const server = Fastify({ logger: true });
+  server.get("/", (_req, _reply) => {
+    return {
+      message: "Hello World, Running With TSX gain",
+    };
+  });
+
+  void (await registerClassRoutes(server));
+  const start = async (server: FastifyInstance, port: number) => {
+    try {
+      void (await server.listen({ port }));
+    } catch (error) {
+      console.error(error);
+      process.exit(2);
+    }
+  };
+  return { server, start, port: PORT };
 }
