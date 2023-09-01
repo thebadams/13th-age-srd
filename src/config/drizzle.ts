9aba1f8f6b530c5b27ac7eadedb9675d8d85d94c
thebadams <bpeteradams@gmail.com>
2023-08-31 20:30:27 -0400

    Cleanup: some cleanup of files and added logging to drizzle orm

--- src/config/drizzle.ts
+++ src/config/drizzle.ts
@@ -1,8 +1,8 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
-import { config } from "dotenv";
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { config } from 'dotenv';
 config();
 
 const client = postgres(process.env.DATABASE_URL as string);
 
-export const drizzleDB = drizzle(client);
+export const drizzleDB = drizzle(client, { logger: true });
